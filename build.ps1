bc806dc2b00e97421f51af967d9cb9d6b491ec6e
Paul Hatcher <paulh@atrios.co.uk>
2015-10-06 18:07:05 +0100

    Add Powershell invoker for Fake so we can call from VSO vNext.Build

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(mandatory = $true, position = 0)][string]$Target,
+    [Parameter(mandatory = $false, position = 1, ValueFromRemainingArguments=$true)]$Remaining
+)
+
+if (-Not (Test-Path packages\FAKE\tools\fake.exe))
+{
+    nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+}
+
+. "packages\FAKE\tools\fake.exe" "build.fsx" "target=$Target" @Remaining
